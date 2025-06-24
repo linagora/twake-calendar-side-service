2a6a57b50c45bca9ee7fa6bc23bf6ea0659c43f0
hung phan <hphan@linagora.com>
2025-06-24 14:57:50 +0700

    ISSUE-127 Add EventEmailReconnectionHandler

---
+++ calendar-amqp/src/main/java/com/linagora/calendar/amqp/EventEmailReconnectionHandler.java
@@ -0,0 +1,53 @@
+/********************************************************************
+ *  As a subpart of Twake Mail, this file is edited by Linagora.    *
+ *                                                                  *
+ *  https://twake-mail.com/                                         *
+ *  https://linagora.com                                            *
+ *                                                                  *
+ *  This file is subject to The Affero Gnu Public License           *
+ *  version 3.                                                      *
+ *                                                                  *
+ *  https://www.gnu.org/licenses/agpl-3.0.en.html                   *
+ *                                                                  *
+ *  This program is distributed in the hope that it will be         *
+ *  useful, but WITHOUT ANY WARRANTY; without even the implied      *
+ *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR         *
+ *  PURPOSE. See the GNU Affero General Public License for          *
+ *  more details.                                                   *
+ ********************************************************************/
+
+package com.linagora.calendar.amqp;
+
+import java.time.Duration;
+
+import jakarta.inject.Inject;
+
+import org.apache.james.backends.rabbitmq.SimpleConnectionPool;
+import org.reactivestreams.Publisher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.rabbitmq.client.Connection;
+
+import reactor.core.publisher.Mono;
+
+public class EventEmailReconnectionHandler implements SimpleConnectionPool.ReconnectionHandler {
+    private static final Logger LOGGER = LoggerFactory.getLogger(EventEmailReconnectionHandler.class);
+    private static final Duration DELAY_ON_COMPLETED = Duration.ofSeconds(30);
+
+    private final EventEmailConsumer eventEmailConsumer;
+
+    @Inject
+    public EventEmailReconnectionHandler(EventEmailConsumer eventEmailConsumer) {
+        this.eventEmailConsumer = eventEmailConsumer;
+    }
+
+    @Override
+    public Publisher<Void> handleReconnection(Connection connection) {
+        return Mono.fromRunnable(eventEmailConsumer::restart)
+            .doOnError(error -> LOGGER.error("Error while handle reconnection for email consumer", error))
+            .then(Mono.delay(DELAY_ON_COMPLETED))
+            .then();
+    }
+}
+
