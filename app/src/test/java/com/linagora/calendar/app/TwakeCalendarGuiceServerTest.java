9f2a242fecd55a4878b072b2ea26d27f59d84633
Benoit TELLIER <btellier@linagora.com>
2025-07-28 20:49:13 +0200

    ISSUE-261 /api/users?email=XXX auto provisioning

--- app/src/test/java/com/linagora/calendar/app/TwakeCalendarGuiceServerTest.java
+++ app/src/test/java/com/linagora/calendar/app/TwakeCalendarGuiceServerTest.java
@@ -822,6 +822,51 @@ class TwakeCalendarGuiceServerTest  {
     }
 
     @Test
+    void userByEmailEndpointShouldProvisionMissingUsers(TwakeCalendarGuiceServer server) {
+        targetRestAPI(server);
+        Username username = Username.of("to-be-provisionned@linagora.com");
+        server.getProbe(CalendarDataProbe.class).addUserToRepository(username, "123456");
+        String domainId = server.getProbe(CalendarDataProbe.class).domainId(DOMAIN).value();
+
+        String body = given()
+            .auth().preemptive().basic(USERNAME.asString(), PASSWORD)
+            .queryParam("email", username.asString())
+        .when()
+            .get("/api/users")
+        .then()
+            .statusCode(200)
+            .extract()
+            .body()
+            .asString();
+        String userId = server.getProbe(CalendarDataProbe.class).userId(username).value();
+
+        assertThatJson(body)
+            .isEqualTo(String.format("""
+                [{
+                    "preferredEmail": "btellier@linagora.com",
+                    "_id": "%s",
+                    "state": [],
+                    "domains": [
+                        {
+                            "domain_id": "%s",
+                            "joined_at": "1970-01-01T00:00:00.000Z"
+                        }
+                    ],
+                    "main_phone": "",
+                    "followings": 0,
+                    "following": false,
+                    "followers": 0,
+                    "emails": [
+                        "btellier@linagora.com"
+                    ],
+                    "firstname": "btellier@linagora.com",
+                    "lastname": "btellier@linagora.com",
+                    "objectType": "user"
+                }]
+                """, userId, domainId));
+    }
+
+    @Test
     void getUserShouldReturnProfile(TwakeCalendarGuiceServer server) {
         targetRestAPI(server);
         String domainId = server.getProbe(CalendarDataProbe.class).domainId(DOMAIN).value();
