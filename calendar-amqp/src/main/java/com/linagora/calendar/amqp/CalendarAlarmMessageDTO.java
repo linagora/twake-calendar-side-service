6577109497a246a7f529942ac5a28e519aa7e62c
hung phan <hphan@linagora.com>
2025-07-29 10:33:21 +0700

    ISSUE-193 Setup rabbitmq consumer for alarm messsage

---
+++ calendar-amqp/src/main/java/com/linagora/calendar/amqp/CalendarAlarmMessageDTO.java
@@ -0,0 +1,28 @@
+/********************************************************************
+ *  As a subpart of Twake Mail, this file is edited by Linagora.    *
+ *                                                                  *
+ *  https://twake-mail.com/                                         *
+ *  https://linagora.com                                            *
+ *                                                                  *
+ *  This file is subject to The Affero Gnu Public License           *
+ *  version 3.                                                      *
+ *                                                                  *
+ *  https://www.gnu.org/licenses/agpl-3.0.en.html                   *
+ *                                                                  *
+ *  This program is distributed in the hope that it will be         *
+ *  useful, but WITHOUT ANY WARRANTY; without even the implied      *
+ *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR         *
+ *  PURPOSE. See the GNU Affero General Public License for          *
+ *  more details.                                                   *
+ ********************************************************************/
+
+package com.linagora.calendar.amqp;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.linagora.calendar.storage.CalendarURL;
+
+public record CalendarAlarmMessageDTO(@JsonProperty("eventPath") String eventPath) {
+    public CalendarURL extractCalendarURL() {
+        return EventFieldConverter.extractCalendarURL(eventPath);
+    }
+}
