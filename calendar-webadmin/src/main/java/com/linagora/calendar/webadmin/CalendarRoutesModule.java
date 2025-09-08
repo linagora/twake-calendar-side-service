8d37f8034a9dc6e91e756a6ff4045a967aaaa8d7
Benoit TELLIER <btellier@linagora.com>
2025-09-08 11:31:23 +0200

    ISSE-316 Bind ResourceRoutes

--- calendar-webadmin/src/main/java/com/linagora/calendar/webadmin/CalendarRoutesModule.java
+++ calendar-webadmin/src/main/java/com/linagora/calendar/webadmin/CalendarRoutesModule.java
@@ -42,6 +42,7 @@ public class CalendarRoutesModule extends AbstractModule {
         routesMultibinder.addBinding().to(CalendarUserRoutes.class);
         routesMultibinder.addBinding().to(CalendarChannelLogoutRoutes.class);
         routesMultibinder.addBinding().to(CalendarRoutes.class);
+        routesMultibinder.addBinding().to(ResourceRoutes.class);
 
         bind(MemoryTaskManager.class).in(Scopes.SINGLETON);
         bind(TaskManager.class).to(MemoryTaskManager.class);
