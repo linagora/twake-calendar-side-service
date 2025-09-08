5146796f994651abcd23f7e64c4f630306ef265d
hung phan <hphan@linagora.com>
2025-09-08 16:21:21 +0700

    fixup! ISSUE-313 Implement EventResourceConsumer - RabbitMQ Setup

--- calendar-amqp/src/main/java/com/linagora/calendar/amqp/EventResourceHandler.java
+++ calendar-amqp/src/main/java/com/linagora/calendar/amqp/EventResourceHandler.java
@@ -18,19 +18,27 @@
 
 package com.linagora.calendar.amqp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import reactor.core.publisher.Mono;
 
 public class EventResourceHandler {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(EventResourceHandler.class);
+
     public Mono<Void> handleCreateEvent(CalendarResourceMessageDTO message) {
+        LOGGER.debug("Handle create event with resource message containing resourceId {} and eventPath {}", message.resourceId(), message.eventPath());
         return Mono.empty();
     }
 
     public Mono<Void> handleAcceptEvent(CalendarResourceMessageDTO message) {
+        LOGGER.debug("Handle accept event with resource message containing resourceId {} and eventPath {}", message.resourceId(), message.eventPath());
         return Mono.empty();
     }
 
     public Mono<Void> handleDeclineEvent(CalendarResourceMessageDTO message) {
+        LOGGER.debug("Handle decline event with resource message containing resourceId {} and eventPath {}", message.resourceId(), message.eventPath());
         return Mono.empty();
     }
 }
