de6b56dbdf3329bad1438991e3c28909a0c04d9c
Benoit TELLIER <btellier@linagora.com>
2025-11-28 23:55:55 +0100

    fixup! fixup! fixup! ISSUE-503 Implement PATCH /api/configurations

--- calendar-rest-api/src/main/java/com/linagora/calendar/restapi/routes/UserConfigurationPatchRoute.java
+++ calendar-rest-api/src/main/java/com/linagora/calendar/restapi/routes/UserConfigurationPatchRoute.java
@@ -19,7 +19,6 @@
 package com.linagora.calendar.restapi.routes;
 
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
