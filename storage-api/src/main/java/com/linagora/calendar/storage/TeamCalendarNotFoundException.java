eee2a7cf942f870cc701a22d02272410a83e46a9
vttran <vttran@linagora.com>
2026-06-27 03:29:38 +0700

    issues/829 [Team calendar] Add entity repository API and memory implementation (#857)

---
+++ storage-api/src/main/java/com/linagora/calendar/storage/TeamCalendarNotFoundException.java
@@ -0,0 +1,27 @@
+/********************************************************************
+ *  As a subpart of Twake Mail, this file is edited by Linagora.    *
+ *                                                                  *
+ *  https://twake-mail.com/                                         *
+ *  https://linagora.com                                            *
+ *                                                                  *
+ *  This file is subject to The Affero Gnu Public License           *
+ *  version 3.                                                      *
+ *                                                                  *
+ *  https://www.gnu.org/licenses/agpl-3.0.en.html                   *
+ *                                                                  *
+ *  This program is distributed in the hope that it will be         *
+ *  useful, but WITHOUT ANY WARRANTY; without even the implied      *
+ *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR         *
+ *  PURPOSE. See the GNU Affero General Public License for          *
+ *  more details.                                                   *
+ ********************************************************************/
+
+package com.linagora.calendar.storage;
+
+import com.linagora.calendar.storage.model.TeamCalendarId;
+
+public class TeamCalendarNotFoundException extends RuntimeException {
+    public TeamCalendarNotFoundException(TeamCalendarId id) {
+        super("Team calendar not found: " + id.value());
+    }
+}
---
+++ storage-api/src/main/java/com/linagora/calendar/storage/model/TeamCalendarId.java
@@ -0,0 +1,38 @@
+/********************************************************************
+ *  As a subpart of Twake Mail, this file is edited by Linagora.    *
+ *                                                                  *
+ *  https://twake-mail.com/                                         *
+ *  https://linagora.com                                            *
+ *                                                                  *
+ *  This file is subject to The Affero Gnu Public License           *
+ *  version 3.                                                      *
+ *                                                                  *
+ *  https://www.gnu.org/licenses/agpl-3.0.en.html                   *
+ *                                                                  *
+ *  This program is distributed in the hope that it will be         *
+ *  useful, but WITHOUT ANY WARRANTY; without even the implied      *
+ *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR         *
+ *  PURPOSE. See the GNU Affero General Public License for          *
+ *  more details.                                                   *
+ ********************************************************************/
+
+package com.linagora.calendar.storage.model;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.google.common.base.Preconditions;
+import com.linagora.calendar.storage.OpenPaaSId;
+
+public record TeamCalendarId(String value) {
+    public TeamCalendarId {
+        Preconditions.checkArgument(!StringUtils.isBlank(value), "team calendar id must not be empty");
+    }
+
+    public static TeamCalendarId from(OpenPaaSId openPaaSId) {
+        return new TeamCalendarId(openPaaSId.value());
+    }
+
+    public OpenPaaSId asOpenPaaSId() {
+        return new OpenPaaSId(value);
+    }
+}
